3d0da10c076df05b6da9b8bc95865b840c1f1348
Fedotov Aleksandr <gaer87@gmail.com>
2016-03-11 23:59:35 +0300

    Update an example

--- develop/app.js
+++ develop/app.js
@@ -1,17 +1,11 @@
 /* Model */
-var Message = Backbone.Model.extend({
-
-});
-
-var Messages = Backbone.Collection.extend({
-	model: Message
-});
-
-var messages = new Messages([
+var data = [
 	{text: 'Best part of the day ☕', date: '5.03.2016'},
 	{text: 'What\'s everybody reading?', date: '3.03.2016'},
 	{text: 'End of summer reading list', date: '1.03.2016'}
-]);
+];
+
+var messages = new Backbone.Collection(data);
 
 
 /* View */
@@ -36,3 +30,6 @@ var MessagesView = Backbone.Marionette.CollectionView.extend({
 var messagesView = new MessagesView({el: '.js-content', collection: messages});
 messagesView.render();
 
+$('.js-repeat').click(function () {
+	messages.reset(data);
+});
